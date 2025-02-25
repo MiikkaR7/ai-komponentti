2b1a6910786469f161b854e8bfe08718b01fa9d9
MiikkaR7 <miikkariipi22@gmail.com>
2025-02-25 14:32:26 +0200

    fix: fix scrolling ai response to stop autoscrolling if user interacts

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -26,6 +26,14 @@ module.exports = {
                         presets: [
                             "@babel/preset-env",
                             ["@babel/preset-react", {"runtime": "automatic"}]
+                        ],
+                        plugins: [
+                            [
+                                "@babel/plugin-proposal-optional-chaining-assign", 
+                                {
+                                    "version": "2023-07" 
+                                }
+                            ]
                         ]
                     }
                 }
